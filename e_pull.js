ba7c1fb414cd503cdeec212a70bdcd1f66736dfc
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2021-01-23 21:15:58 -0500

    update

---
+++ e_pull.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv';
+dotenv.config();
+
+// .. client setup (keep reading))
+
+console.log(process.env.A);
+console.log(process.env.B);
+clientInformation.login(process.env.TOKEN);
+
